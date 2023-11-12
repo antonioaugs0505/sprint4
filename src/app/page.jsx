7df683aa327419cb7a15394144cb86ebc91bde32
CharCarvalho <charlescarvalho97@outlook.com>
2023-11-11 22:45:38 -0300

    Primeira parte da lading page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,19 @@ import Link from "next/link"
 export default function Home() {
   return (
    
+    <main>
+      <section>
+        <div>
+          <h3>ModalSure</h3>
+          <p>O novo método em solicitação de guinchos. Com a nossa tecnologia integrada, automatização e eficiência, o guincho ideal para sua demanda chegará no menor tempo!</p>
+          <Link href='/'>SOLICITAR GUINCHO</Link>
+        </div>
+
+        <figure>
+          <Image src='/images/guincho.png' width={600} height={450} alt="Guincho"></Image>
+        </figure>
+      </section>
+    </main>
 
 
   )
