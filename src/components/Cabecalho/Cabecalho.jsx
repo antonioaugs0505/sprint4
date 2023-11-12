3faa38a03e26dc9947c6bd30325be289268f796f
CharCarvalho <charlescarvalho97@outlook.com>
2023-11-12 13:18:06 -0300

    Terminando home

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -7,7 +7,7 @@ export default function Cabecalho() {
 
         <Link href="/"><Image className="w-[16vw]" src="/images/logo-porto.png" alt="Logo" width={360} height={120}></Image></Link>
 
-        <Link href="/" className="text-white  no-underline font-semibold text-base md:text-lg lg:text-xl hover:text-blue-700">MEMBROS</Link>
+        <Link href="/" className="text-white no-underline font-semibold text-base md:text-lg lg:text-xl hover:text-blue-700">MEMBROS</Link>
     </nav>
 
   );
