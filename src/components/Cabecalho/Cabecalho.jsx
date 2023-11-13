7b388311085575bed24cd266d489239561cbe955
CamillaSantana <rm99491@fiap.com.br>
2023-11-12 21:01:41 -0300

    Responsivo

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -6,10 +6,11 @@ export default function Cabecalho() {
   return (
     <nav className={styles.menu}>
 
-        <Link href="/"><Image className={styles.logoMenu} src="/images/logo-porto.png" alt="Logo" width={360} height={120}></Image></Link>
+        <Link href="/"><Image className={styles.logoMenu} src="/images/logo-porto.png" alt="Logo" width={300} height={120}></Image></Link>
 
         <Link href="/membros" className={styles.menuItem}>MEMBROS</Link>
     </nav>
 
   );
 }
+
