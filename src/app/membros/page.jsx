4c3cf44cf12ce55d211d3c130491c60362f3c389
antonioaugs0505 <rm550344@fiap.com.br>
2023-11-13 23:12:16 -0300

    Finalizando

--- src/app/membros/page.jsx
+++ src/app/membros/page.jsx
@@ -3,17 +3,19 @@ import styles from "../../styles/Membros.module.css"
 
 
 export default function Membros(){
-    return(
-        <main className={styles.membros}>
-            <div>
-                <p>550113 - Charles Pinto da Silveira Carvalho</p>
-                <p>550344 - Antonio Augusto Gomes dos Santos</p>
-                <p>99354 - Raphael Torres Gonçalves</p>
-                <p>99491 - Camilla Ribeiro Santana</p>
-                <p>94156 - Luan Ribeiro Dias</p>
-            </div>
+    return (
+      <main className={styles.membros}>
+        <div>
+          <p>550113 - Charles Pinto da Silveira Carvalho</p>
+          <p>550344 - Antonio Augusto Gomes dos Santos</p>
+          <p>99354 - Raphael Torres Gonçalves</p>
+          <p>99491 - Camilla Ribeiro Santana</p>
+          <p>94156 - Luan Ribeiro Dias</p>
+        </div>
 
-        <Link href='/' className={styles.voltar}>VOLTAR PARA HOME</Link>
-        </main>
-    )
+        <Link href="/" className={styles.voltar}>
+          VOLTAR PARA HOME
+        </Link>
+      </main>
+    );
 }
